7c34fba69912bbd8b48a10ef13d4d0ec6d4ef987
AdityaD-2512 <dwivediaditya121@gmail.com>
2026-02-20 00:44:49 +0530

    resolve some issues

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,5 +1,5 @@
 import NavBar from './NavBar';
-import Header from './header';
+import Header from './Header';
 import Sales  from './Sales';
 import ThisMonth from './ThisMonth';
 // import LikeButton from './LikeButton';
